3b121c1eab77abe101880a4bb7b41e62de2f2908
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-11-25 12:32:26 +1100

    Updated doc.go

--- doc.go
+++ doc.go
@@ -2,14 +2,16 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package mtgox implements a complete MtGox streaming API client.
-//
-// Overview
-//
-// The Client type represents a connection to the Mt.Gox streaming API using
-// WebSockets (Powered by the modern Gorilla WebSocket package). After creating
-// a Client instance, you should call client.Start to begin handling received
-// messages.
-//
+/*
+  Package mtgox implements a complete MtGox streaming API client.
+
+  Overview
+
+  The Client type represents a connection to the Mt.Gox streaming API using
+  WebSockets (Powered by the modern Gorilla WebSocket package). After creating
+  a Client instance, you should call client.Start to begin handling received
+  messages.
+
+*/
 
 package mtgox
